b13341fbe6a86b070d1fa3e0bd4f59a230aa3621
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-04 19:04:10 +0200

    created Header component

---
+++ src/shared/ui/Header/Header.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react"
+import ThemeSwitcher from "../ThemeSwitcher"
+
+const Header: FC = () => {
+    return (
+        <header>
+           <ThemeSwitcher/> 
+        </header>
+    )
+}
+
+export default Header
