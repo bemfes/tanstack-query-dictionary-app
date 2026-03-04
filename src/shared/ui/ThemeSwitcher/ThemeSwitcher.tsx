8a81c11ff820d5af37e4329e2069835478b2740d
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-04 20:57:47 +0200

    added icon moon and class to button in ThemeSwitcher component

--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,13 +1,16 @@
 import { type FC } from "react"
 import Button from "../Button"
 import { useThemeContext } from "@/shared/hooks/useThemeContext/useThemeContext"
+import IconMoon from '@/shared/assets/icon-moon.svg?react'
+import './ThemeSwitcher.css'
 
 
 const ThemeSwitcher: FC = () => {
-    const { changeTheme } = useThemeContext()
+    const { changeTheme, theme } = useThemeContext()
     return (
-        <div>
-            <Button onClick={changeTheme}>click</Button>
+        <div className="switcher-box">
+            <Button className={`${theme === 'dark' ? 'switcher-btn_dark' : ''} switcher-btn btn`} onClick={changeTheme}></Button>
+            <IconMoon className="icon-moon"/>
         </div>
     )
 }
