e3d330633a7502d00b90a2d29857149b1c37acb1
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-04 20:58:57 +0200

    added icon book and class for styles to Header component

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,9 +1,12 @@
 import type { FC } from "react"
 import ThemeSwitcher from "../ThemeSwitcher"
+import './Header.css'
+import IconBook from '@/shared/assets/icon-book.svg?react'
 
 const Header: FC = () => {
     return (
-        <header>
+        <header className="header">
+            <IconBook className="icon-book"/>
            <ThemeSwitcher/> 
         </header>
     )
