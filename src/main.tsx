cae800601a85a0463d4d54e0c8aaed491be498ff
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-07 11:43:05 +0200

    wrapped App component in Error Boundary component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/app/App.tsx'
+import { ErrorBoundary } from 'react-error-boundary'
 
 createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary fallback={<p>There seems to have been an error, please try refreshing the page.</p>}>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </ErrorBoundary>,
 )
