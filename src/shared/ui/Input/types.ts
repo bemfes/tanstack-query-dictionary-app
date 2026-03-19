345cf3786a82daa64261fc572420899cf5997170
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-19 13:29:35 +0200

    created types for Input component

---
+++ src/shared/ui/Input/types.ts
@@ -0,0 +1,7 @@
+export interface InputProps {
+    type: string;
+    placeholder?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+    className?: string;
+}
