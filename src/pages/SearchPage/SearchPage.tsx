2832b2946a855576d9ebba4d563f1a170b7c7cba
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-20 18:16:47 +0200

    created SearchPage with widget Searchord

---
+++ src/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,10 @@
+import SearchWord from "@/widgets/SearchWord/SearchWord"
+import type { FC } from "react"
+
+const SearchPage: FC = () => {
+    return (     
+        <SearchWord/>
+    )
+}
+
+export default SearchPage
