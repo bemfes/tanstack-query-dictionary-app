07e128a6b222b88c360ac5b8adca233937381721
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-20 18:05:27 +0200

    added value prop to Input component in shared

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,9 +1,9 @@
 import type { FC } from "react"
 import type { InputProps } from "./types"
 
-const Input: FC<InputProps> = ({type, placeholder, onChange, className, onKeyDown}) => {
+const Input: FC<InputProps> = ({type, placeholder, onChange, className, onKeyDown, value}) => {
     return (
-        <input onKeyDown={onKeyDown} className={className} onChange={onChange} type={type} placeholder={placeholder} />
+        <input value={value} onKeyDown={onKeyDown} className={className} onChange={onChange} type={type} placeholder={placeholder} />
     )
 }
 
