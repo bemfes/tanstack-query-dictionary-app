d5b99cff32f7617e2facb6d28d9a14ac0f57ba1b
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-20 18:15:07 +0200

    created widget SearchWord to get word search results from the API

---
+++ src/widgets/SearchWord/SearchWord.tsx
@@ -0,0 +1,63 @@
+import SearchInput from "@/features/Search/SearchInput/SearchInput";
+import SearchResult from "@/features/Search/SearchResult/SearchResult";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+const SearchWord = () => {
+  
+    const [searchValue, setSearchValue] = useState<string>('')
+
+    const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement >) => {
+        const value = e.target.value.toLowerCase()
+        setSearchValue(value)
+    }
+    const onClickButtonSearch = () => {
+        refetch()
+    }
+    const onClickButtonDeleteSearchValue = () => {
+        setSearchValue('')
+    }
+    const onKeyDownSearchInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            refetch()
+        }
+        
+    }
+    const getData = async function() {
+        try {
+            const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`)
+            if (!res.ok) {
+                throw new Error('failed to fetch')
+            }
+            const data = await res.json()
+            console.log(data);
+            return data
+        } catch (error) {
+            console.log(error);
+        }
+        
+
+    }
+    const { data, isLoading, error, refetch } = useQuery({ queryKey: ['search'], queryFn: getData,
+         enabled: false,
+         retry: false,
+     })
+    
+    return (
+        <>
+            <SearchInput 
+             inputValue={searchValue} 
+             onKeyDownSearchInput={onKeyDownSearchInput}
+             onChangeSearchInput={onChangeSearchInput}
+             onClickButtonSearch={onClickButtonSearch}
+             onClickButtonDeleteSearchValue={onClickButtonDeleteSearchValue}
+             />
+            
+            <SearchResult data={data} isLoading={isLoading} error={error}/>
+            
+
+        </>
+    )
+}
+
+export default SearchWord
