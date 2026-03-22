32563828a980afe2d21c512923d80c386d067a6c
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-22 11:38:06 +0200

    added classnames to Search Input feature

--- src/features/Search/SearchInput/SearchInput.tsx
+++ src/features/Search/SearchInput/SearchInput.tsx
@@ -2,16 +2,20 @@ import Button from "@/shared/ui/Button"
 import Input from "@/shared/ui/Input"
 import type { FC } from "react"
 import type { SearchInputProps } from "./types"
+import "./SearchInput.css"
 
 
 const SearchInput: FC<SearchInputProps> = ({ inputValue, onChangeSearchInput, onKeyDownSearchInput,
     onClickButtonSearch, onClickButtonDeleteSearchValue
  }) => {
     return (
-        <div>
-            <Input value={inputValue} onKeyDown={onKeyDownSearchInput} onChange={onChangeSearchInput} type="search" placeholder="Search..."/>
-            <Button onClick={onClickButtonDeleteSearchValue}>delete</Button>    
-            <Button onClick={onClickButtonSearch}>search</Button>
+        <div className="search-input__box">
+            <div className="search-input__content">
+                <Input className="search-input" value={inputValue} onKeyDown={onKeyDownSearchInput} onChange={onChangeSearchInput} type="search" placeholder="Search..."/>
+                <Button onClick={onClickButtonDeleteSearchValue}>delete</Button>    
+                <Button onClick={onClickButtonSearch}>search</Button>
+            
+            </div>
         </div>
     )
 }
