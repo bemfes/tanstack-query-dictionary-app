6b12cc8444bea9b6fed93864d79b2bb338b8b842
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-22 11:53:12 +0200

    added basic html structure to Search Result feature

--- src/features/Search/SearchResult/SearchResult.tsx
+++ src/features/Search/SearchResult/SearchResult.tsx
@@ -1,11 +1,36 @@
 import type { FC } from "react";
 import type { SearchResultProps } from "./types";
+import type { DictionaryWordType } from "@/shared/types/DictionaryWordType";
 
 const SearchResult: FC<SearchResultProps> = ({ data, isLoading, error }) => {
     return (
         <div>
             {isLoading && <p>Loading...</p>}
-            {!isLoading && data && !error && <p>data</p>}
+            {!isLoading && data && !error && <div>
+                {
+                    data.map( (item: DictionaryWordType, index) => (
+                        <div key={index}>
+                            <p>{item.word}</p>
+                            <p>{item.phonetic}</p>
+                            <div>
+                                {item.meanings.map(meaning => (
+                                    <>
+                                    <p>{meaning.partOfSpeech}</p>
+                                    <div>
+                                        {meaning.definitions.map(defs => (
+                                            <p>{defs.definition}</p>
+                                        ))}
+                                    </div>
+                                    </>
+                                    
+                                ))}
+                            </div>
+                        </div>
+                    ))
+                }
+               
+                
+            </div>}
             { error && <p>{error.message}</p> }
         </div>
     )
