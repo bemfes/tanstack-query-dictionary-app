d8d28353e0e4a2d2f61447ce9e3c86762261a212
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-22 10:52:35 +0200

    made ts interfaces for License and Meaning for Dictionary Word Interface in types shared

--- src/shared/types/DictionaryWordType/DictionaryWordType.ts
+++ src/shared/types/DictionaryWordType/DictionaryWordType.ts
@@ -1,3 +1,17 @@
+interface License {
+    name: string;
+    url: string;
+}
+
+interface Meaning {
+    partOfSpeech: string;
+    definitions: Definition[];
+    synonyms: string[];
+    antonyms: string[]
+}
+
 export interface DictionaryWordType {
+    license: License;
+    meanings: Meaning[]
     word: string;
 }
