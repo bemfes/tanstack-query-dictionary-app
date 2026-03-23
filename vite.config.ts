5c39a8dcc976e025861336b2fed29cc4edb2eaea
Anastasiia Ustin <2anastasiia@mail.ru>
2026-03-23 21:29:38 +0200

    added base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/dictionary-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
