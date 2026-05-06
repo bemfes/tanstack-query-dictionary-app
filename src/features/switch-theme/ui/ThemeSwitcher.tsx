69328a7edabc350d02bc1226e0cc253ba82c3e1b
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-06 17:40:21 +0200

    refactor: move theme switcher component to its feature

---
+++ src/features/switch-theme/ui/ThemeSwitcher.tsx
@@ -0,0 +1,20 @@
+import { type FC } from "react";
+import Button from "../../../shared/ui/Button";
+import { useThemeContext } from "@/features/switch-theme/model/useThemeContext";
+import IconMoon from "@/shared/assets/icon-moon.svg?react";
+import "./ThemeSwitcher.css";
+
+const ThemeSwitcher: FC = () => {
+  const { changeTheme, theme } = useThemeContext();
+  return (
+    <div className="switcher-box">
+      <Button
+        className={`${theme === "dark" ? "switcher-btn_dark" : ""} switcher-btn btn`}
+        onClick={changeTheme}
+      ></Button>
+      <IconMoon className="icon-moon" />
+    </div>
+  );
+};
+
+export default ThemeSwitcher;
