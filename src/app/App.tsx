32c52d1163be3dccba02fb6c80b7b97818912a71
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-06 18:05:27 +0200

    refactor: delete tanstack provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,12 @@
 import "./index.css";
 import Header from "@/shared/ui/Header";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchPage from "@/pages/SearchPage";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <Header />
-      <QueryClientProvider client={queryClient}>
-        <SearchPage />
-      </QueryClientProvider>
+      <SearchPage />
     </>
   );
 }
