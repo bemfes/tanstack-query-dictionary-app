11664d40145ac27758ae330c04e70d2c871983dd
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-06 18:00:25 +0200

    refactor: delete error boundary

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ErrorBoundary } from "react-error-boundary";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
-  <ErrorBoundary
-    fallback={
-      <p>There seems to have been an error, please try refreshing the page.</p>
-    }
-  >
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </ErrorBoundary>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
