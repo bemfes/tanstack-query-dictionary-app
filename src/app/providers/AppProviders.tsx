80dcc0d24caf778628b50d0492c89e6e2f3081f3
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-06 17:59:37 +0200

    refactor: add error boundary

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,7 +1,18 @@
 import ThemeContextProvider from "@/features/switch-theme/model";
+import { ErrorBoundary } from "react-error-boundary";
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
-  return <ThemeContextProvider>{children}</ThemeContextProvider>;
+  return (
+    <ErrorBoundary
+      fallback={
+        <p>
+          There seems to have been an error, please try refreshing the page.
+        </p>
+      }
+    >
+      <ThemeContextProvider>{children}</ThemeContextProvider>
+    </ErrorBoundary>
+  );
 };
 
 export default AppProviders;
