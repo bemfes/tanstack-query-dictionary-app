e1fc257fb915b6700fb25234c38fec239f662c01
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-06 17:11:37 +0200

    refactor: update path for App component

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "@/app/App.tsx";
 import { ErrorBoundary } from "react-error-boundary";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary
