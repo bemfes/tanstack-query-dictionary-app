e88c145a05704a5ab4cbceaa2517f60f44c44e8c
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-07 19:20:45 +0200

    refactor: import variables from useSearchWord hook

--- src/features/search-word/ui/SearchWord/SearchWord.tsx
+++ src/features/search-word/ui/SearchWord/SearchWord.tsx
@@ -1,33 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
 import SearchInput from "../SearchInput";
 import SearchResult from "../SearchResult";
-import { getWord } from "../../api";
+import { useSearchWord } from "../../model";
 
 const SearchWord = () => {
-  const [searchValue, setSearchValue] = useState<string>("keyboard");
-
-  const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.toLowerCase();
-    setSearchValue(value);
-  };
-  const onClickButtonSearch = () => {
-    refetch();
-  };
-  const onClickButtonDeleteSearchValue = () => {
-    setSearchValue("");
-  };
-  const onKeyDownSearchInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      refetch();
-    }
-  };
-
-  const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["search"],
-    queryFn: () => getWord(searchValue),
-  });
-
+  const {
+    searchValue,
+    onKeyDownSearchInput,
+    onChangeSearchInput,
+    onClickButtonDeleteSearchValue,
+    onClickButtonSearch,
+    data,
+    isLoading,
+    error,
+  } = useSearchWord();
   return (
     <>
       <SearchInput
