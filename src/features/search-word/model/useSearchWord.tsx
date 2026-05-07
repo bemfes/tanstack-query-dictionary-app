c668933d67ffc8943a0a0252c332306c1f90d877
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-07 19:15:35 +0200

    refactor: move logic from Search Word to its own file

---
+++ src/features/search-word/model/useSearchWord.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { getWord } from "../api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useSearchWord = () => {
+  const [searchValue, setSearchValue] = useState<string>("keyboard");
+
+  const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.toLowerCase();
+    setSearchValue(value);
+  };
+  const onClickButtonSearch = () => {
+    refetch();
+  };
+  const onClickButtonDeleteSearchValue = () => {
+    setSearchValue("");
+  };
+  const onKeyDownSearchInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      refetch();
+    }
+  };
+
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ["search"],
+    queryFn: () => getWord(searchValue),
+  });
+
+  return {
+    searchValue,
+    onChangeSearchInput,
+    onClickButtonSearch,
+    onClickButtonDeleteSearchValue,
+    onKeyDownSearchInput,
+    data,
+    isLoading,
+    error,
+  };
+};
