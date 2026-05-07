f1f98a24559aa55d0125171a392fd6f3c32cf284
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-07 19:05:29 +0200

    refactor: delete async function for word request

--- src/features/search-word/ui/SearchWord/SearchWord.tsx
+++ src/features/search-word/ui/SearchWord/SearchWord.tsx
@@ -21,26 +21,7 @@ const SearchWord = () => {
       refetch();
     }
   };
-  const getData = async function () {
-    try {
-      const res = await fetch(
-        `https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`,
-      );
-      if (!res.ok) {
-        if (res.status === 404) {
-          throw new Error("Word not found (404). Please check your input.");
-        } else {
-          throw new Error(`Error: ${res.status} (${res.statusText})`);
-        }
-      }
-      const data = await res.json();
-      return data;
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        throw new Error(error.message);
-      }
-    }
-  };
+
   const { data, isLoading, error, refetch } = useQuery({
     queryKey: ["search"],
     queryFn: getData,
