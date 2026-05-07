fda0919cb0e41d7ec7136fb1623e3134e3c2bf26
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-07 18:54:22 +0200

    refactor: move SearchInput folder

---
+++ src/features/search-word/ui/SearchInput/types.ts
@@ -0,0 +1,7 @@
+export interface SearchInputProps {
+  inputValue: string;
+  onChangeSearchInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDownSearchInput: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onClickButtonSearch: () => void;
+  onClickButtonDeleteSearchValue: () => void;
+}
