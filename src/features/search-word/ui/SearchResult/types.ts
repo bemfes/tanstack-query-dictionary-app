cedfcd383c8c593b13ae18bf4a2e09a8da851079
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-09 13:52:08 +0200

    feat: add type for isFetching

--- src/features/search-word/ui/SearchResult/types.ts
+++ src/features/search-word/ui/SearchResult/types.ts
@@ -3,5 +3,6 @@ import type { DictionaryWordType } from "../../model";
 export interface SearchResultProps {
   data: DictionaryWordType[] | undefined;
   isLoading: boolean;
+  isFetching: boolean;
   error: Error | null;
 }
